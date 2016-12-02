a675fa718647f80e158f20a3822d1fc246a64226
Alexey <Global@mail.ru>
2016-12-02 20:55:51 +0200

    Migrate, view tasks, layout

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -1,16 +1,24 @@
 <?php
+use App\Task;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
+/**
+ * Вывести список всех задач
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * Добавить новую задачу
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Удалить существующую задачу
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
 });
